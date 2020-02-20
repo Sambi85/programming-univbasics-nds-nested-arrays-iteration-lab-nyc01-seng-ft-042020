d210be1ee52391234a8c0c370bd5ba710ed22ea2
Samuel Lesser <ineedmoreplates@gmail.com>
2020-02-20 21:57:51 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -53,7 +53,8 @@ while row_index < src.count do
   while element_index < src[row_index].count do
     
     if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0 
-    total += src[row_index][0] += src[row_index][1]
+    total += src[row_index][0]
+    total += src[row_index][1]
   end
   
   
